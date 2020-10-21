52e8981861d49279d4fd1ecb383e099667e7ee60
MikailMilissen <mikail.milissen@gmail.com>
2020-10-21 13:31:59 +0200

    pull

---
+++ Projecten/Typewriting/app.js
@@ -0,0 +1,13 @@
+const text = ['website', 'illustrations', 'code', 'products']
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type (){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+}());
