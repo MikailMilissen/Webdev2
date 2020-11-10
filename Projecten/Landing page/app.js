18d04c3b4e5247cfa9cc862373aeadee48ea1fe4
MikailMilissen <mikail.milissen@gmail.com>
2020-11-10 16:50:12 +0100

    ready

---
+++ Projecten/Landing page/app.js
@@ -0,0 +1,8 @@
+const time = document.getElementById('time')
+const greeting = document.getElementById('greeting')
+const name = document.getElementById('name')
+const focus = document.getElementById('focus');
+
+var a = new Date();
+var b = a.getDate();
+console.log(b)
