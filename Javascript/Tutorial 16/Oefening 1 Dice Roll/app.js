c37d55416cdf1364d701dc19d046b028a91ae4bd
MikailMilissen <mikail.milissen@gmail.com>
2020-11-10 12:07:37 +0100

    ready

--- Javascript/Tutorial 16/Oefening 1 Dice Roll/app.js
+++ Javascript/Tutorial 16/Oefening 1 Dice Roll/app.js
@@ -7,6 +7,7 @@
 let button = document.getElementById('diceRollerbutton');
 const diceOne = document.getElementById('placeholder1');
 const diceTwo = document.getElementById('placeholder2');
+let textPlayer = document.getElementById('textPlayer');
 
 // function changeDice
 
@@ -15,6 +16,11 @@ function changeDice(){
     const secondRandomDice = Math.floor(Math.random() * 6) + 1;
     diceOne.innerHTML = `<img src="assets/dice${firstRandomDice}.png" style="height: 100px; width: 100px;">`;
     diceTwo.innerHTML = `<img src="assets/dice${secondRandomDice}.png" style="height: 100px; width: 100px;">`;
+    if (firstRandomDice > secondRandomDice){
+        textPlayer.innerText = `First Player Wins`;
+    } else {
+        textPlayer.innerText = `Second Player Wins`;
+    }
 }
 
 button.addEventListener('click',changeDice)
