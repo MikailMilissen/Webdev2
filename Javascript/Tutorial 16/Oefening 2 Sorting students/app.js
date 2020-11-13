ec2a94ddae8e7dabc7af5b0ee8b947b6f5f1a482
MikailMilissen <mikail.milissen@gmail.com>
2020-11-13 15:09:57 +0100

    ready

--- Javascript/Tutorial 16/Oefening 2 Sorting students/app.js
+++ Javascript/Tutorial 16/Oefening 2 Sorting students/app.js
@@ -1,22 +1,36 @@
 var students = [
-    {
-        name: "Student1",
-        score: 10
-    },
-    {
-        name: "Student2",
-        score: 8
-    },
-
-    {
-        name: "Student3",
-        score: 5
-    },
-    {
-        name: "Student4",
-        score: 3
-    }
+    {name: "Mikail",score: 10},
+    {name: "Susan",score: 8},
+    {name: "Priya",score: 5},
+    {name: "Lotte",score: 3},
+    {name: "Daina",score: 8},
+    {name: "Lorres",score: 4},
+    {name: "Wouter",score: 9},
+    {name: "Morgane",score: 3},
+    {name: "Bruce",score: 0},
+    {name: "Rochtus",score: 6}
 ]
+
+console.log(students)
+
+// Checking Scores above or equal to 5
+const checkScores = students.filter((students) => {
+    return students.score >= 5})
+console.log(checkScores)
+
+//sorting studentd alfabetically with a score higher or equal than 5
+const sortStudents = checkScores.sort((a,b) => {
+    var nameA = a.name;
+    var nameB = b.name;
+    if (nameA<nameB){
+        return -1; 
+    }if (nameA>nameB){
+        return 1;
+    }
+    return 0;
+})
+console.log(sortStudents)
+
 
 /*
 
@@ -35,58 +49,30 @@ getStudentName()
 printerService()
 
 */
-
+// Emmanuem methodes
 // return the score above 5
-function CheckScore(student){
-return student.score >=5
-} 
+            // function CheckScore(student){
+            // return student.score >=5
+            // } 
 
-// return the students
-function getStudentName(student){
-return student.name
-}
-// print the score
-function printerService(data){
-return data
+            // // return the students
+            // function getStudentName(student){
+            // return student.name
+            // }
+            // // print the score
+            // function printerService(data){
+            // return data
 
-.filter(CheckScore).sort((a,b)=>{return a.score - b.score}).map(
-getStudentName)
+            // .filter(CheckScore).sort((a,b)=>{return a.score - b.score}).map(
+            // getStudentName)
 
-} 
- 
-let result = printerService(students);
-console.log(result)
+            // } 
+            
+            // let result = printerService(students);
+            // console.log(result)
 
 /* Emmmanuel antwoord
 console.log(students.filter((val,index)=>val.score>=5)
         .sort((a,b)=>name.a-name.b))
 */
 
-
-
-
-
-/*
-method: some()
-    The some() method checks if any of the elements in an array pass a test
-
-
-function someFunction(value){
-    return value>=100
-}
-
-let some_Result = myNumberArray.some(someFunction)
-console.log(some_Result)
-
-/*
-method: sort()
-    The sort() method sorts the items of an array.
-*/
-/*
-myNumberArray.sort(function(a,b){return b-a});
-var strings= ['f','s','e','l','q','x']
-strings.sort();
-console.log(strings)
-console.log(myNumberArray)
-
-*/
