66afad739128bd31c68b1ac0132685c3170e6deb
MikailMilissen <mikail.milissen@gmail.com>
2020-12-09 15:30:45 +0100

    ready

---
+++ Projecten/Advent of Code/Day 2/app.js
@@ -0,0 +1,45 @@
+/* 6-8 s: svsssszslpsp
+3-4 n: gncn
+4-8 v: vkvmvdmvhttvvrgvvwv
+16-18 j: jjjjjjjjjjjjjjjjjf
+13-15 p: pppppppvppppxxppp
+3-4 k: bkksggqbtwkkkzqcz
+8-18 x: qxphxxtczxxxxxrbxxl
+6-11 c: dccxcccccchrcfdckcsc
+10-11 c: ccvxccccccccc
+2-4 f: pszff
+18-20 z: zzzzzzzzzzwzzzzzzzzj
+1-7 g: ggggggpggggggg
+3-5 h: hhhhfhh
+2-5 x: dxxzxv
+7-8 s: ssssssss
+3-9 k: ktkkkkkklkck
+3-4 n: gncn
+
+var Passwords = {
+    firstValue: 6,
+    lastValue : 8,
+    Value: 's',
+    password: 'svsssszslpsp'
+  }
+
+
+function validPassword(lowNumber,highNumber,Value,Password){
+    this.lowNumber = lowNumber;
+    this.highNumber = highNumber;
+    this.Value = Value;
+    this.Password = Password
+}
+*/
+
+var Arr = [6,8,'s','svsssszslpsp']
+
+
+
+
+let numberOne = Passwords.firstValue
+
+function checkLength(){
+   
+}
+console.log(numberOne)
