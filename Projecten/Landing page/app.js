aae683592dcf2f340a2c61ed851f931c67c3687e
MikailMilissen <mikail.milissen@gmail.com>
2020-12-11 15:31:32 +0100

    ready

--- Projecten/Landing page/app.js
+++ Projecten/Landing page/app.js
@@ -1,8 +1,43 @@
-const time = document.getElementById('time')
-const greeting = document.getElementById('greeting')
-const name = document.getElementById('name')
-const focus = document.getElementById('focus');
+// DOM elements
 
-var a = new Date();
-var b = a.getDate();
-console.log(b)
+const time = document.querySelector('.time')
+const greeting = document.querySelector('#greeting')
+const name = document.querySelector('.name')
+const focus = document.querySelector('.focus');
+
+let today = new Date();
+hour = today.getHours();
+
+function showTime(){
+    minutes = today.getMinutes();
+    seconds = today.getSeconds(); 
+    // output Time
+    time.innerHTML = `${hour}:${addZero(minutes)}:${addZero(seconds)}`;
+    // Let time run every second
+    setTimeout(showTime, 500);
+    // Add a zero to minutes and seconds
+}
+
+function addZero(n){
+    if (n < 10){
+        return `0${n}`
+    } else{
+        return n
+    }
+}
+
+function setGreeting(){
+    if (hour<12){
+        document.body.style.backgroundImage = "url ('../Afbeeldingen/morning.jpg')"
+        greeting.textContent = 'Good Morning'
+    } else if(hour<17){
+        document.body.style.background = "url ('../Afbeeldingen/midday.jpg')"
+        greeting.textContent = 'Good Afternoon'
+    } else {
+        document.body.style.backgroundImage = "url ('../Afbeeldingen/evening.jpg')"
+        greeting.textContent = 'Good Evening'
+    }
+}
+
+showTime()
+setGreeting()
