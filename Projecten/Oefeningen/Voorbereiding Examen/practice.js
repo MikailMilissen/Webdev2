fc4644f6237d1d56932168e90dc72d546cdf26ce
MikailMilissen <mikail.milissen@gmail.com>
2020-12-16 21:51:32 +0100

    ready

--- Projecten/Oefeningen/Voorbereiding Examen/practice.js
+++ Projecten/Oefeningen/Voorbereiding Examen/practice.js
@@ -54,20 +54,102 @@ var car = {
     type: 'Fiat',
     model: '500',
     color: 'white',
-    fullList: function(){
-        console.log(this.type)
-    },
-    brake(){
-        console.log('this car can brake')
-    },
-  
 }
 
 
-car.brake()
-car.fullList()
+function saySomething(){
+    console.log('hello')
+}
+
+var text = 'blabdhqhqheshehioesohiehthsohethstheztgdrgrdgdrgrgrgrddggdrrgseotheshthsetet'
+console.log(text.length)
+
+// document.querySelector('.text').innerHTML = 'hueshuheshsfehuefshuefshiuihsefu'
+//document.querySelector('.text').innerText = 'hueshuheqzdqzzqdsefsefseseffesfesshuefshiuihsefu'
+
+if(x !== 120){console.log('hoera')}else{console.log('nope')}
+
+var str = 'my cat Leo his name is Leo'
+var posFirst = str.indexOf('Leo')
+console.log(posFirst)
+var posLast = str.lastIndexOf('Leo')
+console.log(posLast)
+
+var slice = str.slice(-10,-7)
+console.log(slice)
+
+var textlower = 'This is a lowercase text turning into an uppercase'
+var textUpper = textlower.toUpperCase()
+console.log(textUpper)
+
+console.log(str.charAt(3))
+console.log(str.split(' '))
+
+var Hello = 123;
+var Bye = new Number(123);
+console.log(Bye)
+console.log(x.toString())
+console.log(parseInt('hello'))
+
+var cars = ['Volvo','Saab','BMW',23453]
+
+document.querySelector('.text').innerText = cars[3]
+
+console.log(typeof cars[3])
+console.log(cars.length)
+var last = cars[cars.length-1]
+console.log(last)
+
+function loop(){
+    for (i=0;i<cars.length;i++){
+        console.log(cars[i])
+    }
+}
+
+loop()
+
+document.querySelector('.text').innerText = cars.join(('*Hello'))
+cars.push('dontaddme')
+document.querySelector('.text').innerText = cars.pop()
+
+const boyStudents = ['Atilla','Mikail','Emmanuel']
+const girlStudent = ['Susan','Priya','Lotte']
+const allStudents = boyStudents.concat(girlStudent)
+
+const sortAlpha = () => {
+    allStudents.sort()
+    document.querySelector('.students').innerHTML = allStudents
+}
+
+const sortReversed = () => {
+    allStudents.reverse()
+    document.querySelector('.students').innerHTML = allStudents
+}
+allStudents.reverse()
+console.log(allStudents)
+
+var numbers = [12,24,35,23,46,46]
+numbers.forEach(element => console.log(element))
+
+console.log(Math.round(3.532))
+console.log(Math.floor(Math.random()*100)+1)
+
+var age = Math.floor(Math.random()*70)
+console.log(age)
+const votable = (age<18) ? 'This person is too young':'This person is old enough'
+document.querySelector('.text').innerHTML = `${votable} and the number is ${age}`
 
 
+var i = 0
+function increment(){
+while (i>10){
+    console.log(i);
+    i++;
+}}
+console.log(increment())
 
+var para = document.createElement('p')
+var node = document.createTextNode('This is a new element')
+para.appendChild(node)
 
 
