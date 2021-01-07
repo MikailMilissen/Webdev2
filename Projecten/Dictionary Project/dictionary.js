ad4e8aa5ceee83076c3ea04dbc341cb4a7cb1c6d
MikailMilissen <mikail.milissen@gmail.com>
2021-01-07 12:09:12 +0100

    ready

--- Projecten/Dictionary Project/dictionary.js
+++ Projecten/Dictionary Project/dictionary.js
@@ -6,15 +6,15 @@ const search = document.querySelector('#search')
 const button = document.querySelector('#button')
 const jumbotron = document.querySelector('.jumbotron')
 
-let response = fetch(Api);
-response.then(res=>console.log(res))
-response.then(dic=>dic.json())
-.then(data=>console.log(data))
+// let response = fetch(Api);
+// response.then(res=>console.log(res))
+// response.then(dic=>dic.json())
+// .then(data=>console.log(data))
 
 async function findWord(id){
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${id}`)
     let data = await response.json();
-    console.log(data)
+    return data
     // return JSON.stringify(data)
 }
 
@@ -22,7 +22,7 @@ button.addEventListener('click', (e) => {
     e.preventDefault()
     console.log('test')
     findWord(search.value).then(response=>{
-       console.log(response)
+       response.forEach(e => console.log(e.word))
         })
 })
 
