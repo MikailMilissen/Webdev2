2317d398c0e7e8b8d8d97d4c37245c76e714e99c
MikailMilissen <mikail.milissen@gmail.com>
2021-01-07 17:03:31 +0100

    ready

--- Projecten/CurrencyConverter/app.js
+++ Projecten/CurrencyConverter/app.js
@@ -1,1 +1,16 @@
 // https://api.exchangeratesapi.io/latest?base=EUR&symbol=USD
+const API = ('https://api.exchangeratesapi.io/latest?base=EUR&symbol=USD')
+const enterAmount = document.querySelector('#amount')
+const currencyFrom = document.querySelector('#currencyFrom')
+const currencyTo = document.querySelector('#currencyTo')
+const button = document.querySelector('button')
+const result = document.querySelector('#result')
+
+async function getCurrency(){
+    let result = await fetch('https://api.exchangeratesapi.io/latest?base=EUR&symbol=USD');
+    let data = await result.json()
+    console.log(data)
+    
+}
+
+getCurrency()
--- Projecten/Dictionary Project/dictionary.js
+++ Projecten/Dictionary Project/dictionary.js
@@ -11,9 +11,7 @@ const jumbotron = document.querySelector("#jumbotron");
 // .then(data=>console.log(data))
 
 async function findWord(id) {
-  let response = await fetch(
-    `https://api.dictionaryapi.dev/api/v2/entries/en/${id}`
-  );
+  let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${id}`);
   console.log(response);
   let data = await response.json();
   console.log(data);
@@ -28,18 +26,21 @@ button.addEventListener("click", (e) => {
     response.forEach((e) => {
       return (jumbotron.innerHTML = `
        <h3 class="word-text">${e.word}</h3>
-                    <span class="pronunciation-text" style="color: #808080;">${e.phonetics[0].text}</span>
-                    <div id="definitions-container">
-                        <div class="row" style="border-top: solid 1px #e6e6e6; padding: 10px 0">
-                            <div class="col-sm-9 my-auto"><i>${e.meanings[0].partOfSpeech}<span class="d-inline-block tip" data-toggle="tooltip"
-                                        title="" tabindex="0" data-html="true" data-placement="right"
-                                        data-original-title="Nouns are one of the four major word classes, along with verbs, adjectives and adverbs. Nouns are the largest word class.<br>
-A noun refers to a person, animal, place or thing.
-<a href='#LINK' target='_blank'></a>">?</span></i><br>${e.meanings[0].definitions[0].definition}<br></div>
-                            <div class="col-sm-3 text-right"><img
-                                    src="https://5.imimg.com/data5/WT/GN/ZH/SELLER-3722283/pineapple-500x500.jpg"
-                                    style="width: 110px; max-width: 100%; border-radius: 55px; border: solid 1px #dedede">
-                            </div>
+            <span class="pronunciation-text" style="color: #808080;">${e.phonetics[0].text}</span>
+                <div id="definitions-container">
+                    <div class="row" style="border-top: solid 1px #e6e6e6; padding: 10px 0">
+                        <div class="col-sm-9 my-auto"><i>${e.meanings[0].partOfSpeech}<span class="d-inline-block tip" data-toggle="tooltip"
+                        title="" tabindex="0" data-html="true" data-placement="right"
+                        data-original-title="Nouns are one of the four major word classes, along with verbs, adjectives and adverbs. Nouns are the largest word class.<br>
+                        A noun refers to a person, animal, place or thing.
+                        <a href='#LINK' target='_blank'></a>">?</span></i><br>${e.meanings[0].definitions[0].definition}<br></div>
+                   
+                 
+
+                        <li><a href="${e.phonetics[0].audio}"><i class="fa fa-instagram">🔊</i></li>
+
+
+                 
                         </div>
                     </div>
                 </div>
