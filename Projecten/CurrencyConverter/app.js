f4f7c8a90060ad5f1e9f609e0814490c3585e825
MikailMilissen <mikail.milissen@gmail.com>
2021-01-08 17:27:59 +0100

    ready

--- Projecten/CurrencyConverter/app.js
+++ Projecten/CurrencyConverter/app.js
@@ -6,11 +6,38 @@ const currencyTo = document.querySelector('#currencyTo')
 const button = document.querySelector('button')
 const result = document.querySelector('#result')
 
-async function getCurrency(){
+// const currencies = [AUD,BGN,BRL,CAD,CHF,CNY,CZK,DKK,GBP,HKD,HRK,HUF,IDR,ILS,INR,ISK,JPY,KRW,MXN,MYR,NOK,NZD,PHP,PLN,RON,RUB,SEK,SGD,THB,TRY,USD,ZAR]
+
+async function getData(){
     let result = await fetch('https://api.exchangeratesapi.io/latest?base=EUR&symbol=USD');
     let data = await result.json()
     console.log(data)
-    
+    findCurrency(data)
+}
+// Insert Rates
+
+const findCurrency = (data) => {
+    const rates = data.rates
+    let keys = Object.keys(rates)
+    console.log(keys)
+    keys.forEach(element => {
+        let option = document.createElement('option'); // create a new option element
+        let text = document.createTextNode(element) // create a new text node
+        option.appendChild(text) // append the text to the new option element
+        currencyTo.appendChild(option) // append the option to the element in the HTML file (zie const hierboven)
+    })
+    keys.forEach(element => {
+        let option = document.createElement('option'); // create a new option element
+        let text = document.createTextNode(element) // create a new text node
+        option.appendChild(text) // append the text to the new option element
+        currencyFrom.appendChild(option) // append the option to the element in the HTML file (zie const hierboven)
+    })
 }
 
-getCurrency()
+async function convertRates(){
+    await getData()
+    let amountValue = currencyFrom.value;
+    console.log(amountValue)
+}
+
+convertRates()
