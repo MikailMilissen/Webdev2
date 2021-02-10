8b13bcbf0558507be50d0d1660640e956dc68a54
MikailMilissen <mikail.milissen@gmail.com>
2021-02-10 16:11:15 +0100

    'header'

--- Projecten/tesla/src/App.js
+++ Projecten/tesla/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-     <h1>Tesla Clone App</h1>
+      <Header />
+        {/* items */}
+        {/* items */}
+        {/* items */}
+        {/* items */}
     </div>
   );
 }
